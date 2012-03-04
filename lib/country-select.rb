f576bc71e6313a3fe711969a93f9a7987a6bd7e6
Jared Beck <jared@jaredbeck.com>
2012-03-03 22:57:16 -0500

    The rails 3.2.2 release now requires us to explicity mark our string of options as html_safe

--- lib/country-select.rb
+++ lib/country-select.rb
@@ -2,10 +2,12 @@
 module ActionView
   module Helpers
     module FormOptionsHelper
+
       # Return select and option tags for the given object and method, using country_options_for_select to generate the list of option tags.
       def country_select(object, method, priority_countries = nil, options = {}, html_options = {})
         InstanceTag.new(object, method, self, options.delete(:object)).to_country_select_tag(priority_countries, options, html_options)
       end
+
       # Returns a string of option tags for pretty much any country in the world. Supply a country name as +selected+ to
       # have it marked as the selected option tag. You can also supply an array of countries as +priority_countries+, so
       # that they will be listed above the rest of the (long) list.
@@ -20,14 +22,20 @@ module ActionView
           end
           country_options += options_for_select(priority_countries, selected)
           country_options += "<option value=\"\" disabled=\"disabled\">-------------</option>\n"
-					
+
           # prevents selected from being included twice in the HTML which causes
           # some browsers to select the second selected option (not priority)
           # which makes it harder to select an alternative priority country
           selected = nil if priority_countries.include?(selected)
         end
 
-        return country_options + options_for_select(COUNTRIES, selected)
+        string_of_options = country_options + options_for_select(COUNTRIES, selected)
+
+        # The rails 3.2.2 release now requires us to explicity
+        # mark our string of options as html_safe.  See:
+        # http://bit.ly/x7XeFa
+        # http://bit.ly/A4696B
+        return string_of_options.html_safe
       end
 
       # All the countries included in the country_options output.
@@ -282,7 +290,7 @@ module ActionView
         "Zambia" => "ZM",
         "Zimbabwe" => "ZW"} unless const_defined?("COUNTRIES")
     end
-    
+
     class InstanceTag
       def to_country_select_tag(priority_countries, options, html_options)
         html_options = html_options.stringify_keys
@@ -296,7 +304,7 @@ module ActionView
         )
       end
     end
-    
+
     class FormBuilder
       def country_select(method, priority_countries = nil, options = {}, html_options = {})
         @template.country_select(@object_name, method, priority_countries, options.merge(:object => @object), html_options)
